ea84b3cd1f841cbfbf29925a665cead5501599df
Alex <alexa.exebio@gmail.com>
2025-11-26 20:23:24 +0000

    hacks añadidos

--- generador_dominios.js
+++ generador_dominios.js
@@ -1,20 +1,28 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let domain =['.com','.net','.us','.io']
+let noun = ['jogger', 'morantes','racoon'];
+let domain = ['com', 'net', 'io', 'er', 'es']
+
+//slice substr
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-       for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < domain.length; l++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < domain.length; l++) {
 
-         console.log(pronoun[i],adj[j],noun[k],domain[l]);   
+                let url = pronoun[i] + adj[j] + noun[k];
+                if (url.endsWith(domain[l])) {
+                    let newurl = url.slice(0, (url.length - domain[l].length));
+                    console.log(newurl + "." + domain[l]); //domain l para recorrer cada index del array
 
-        }      
-       }
-       if (pronoun.endsWith) {
-        
-       }
+                } else console.log(pronoun[i] + adj[j] + noun[k] + "." + domain[l]);
+                //con el "+" concatenamos la fucnión en una sola oración       
+
+            }
+        }
+
     }
-    
+
 }
+
+//proyecto hecho en conjunto...dedicado a los tutores Robert y Carlos, y obvio al profe, Javi.
